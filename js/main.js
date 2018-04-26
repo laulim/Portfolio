7e5e8933067b69052305bdc0c507499efc0c81ed
Lyudmila <laudylim@yandex.ru>
2018-04-26 16:27:11 +0300

    Header is ready

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
-	/* Call plugins hrere ...*/
+//PageScroll2Id
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	    highlightSelector:"nav a"
+	});
 
 	
 });
